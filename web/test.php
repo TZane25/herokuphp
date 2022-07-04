0e0ba2edd3eba6e32f4017c2a568f6476b0b1357
TZane25 <20011280@myrp.edu.sg>
2022-07-04 12:42:48 +0800

    commit message goes here

--- web/test.php
+++ web/test.php
@@ -2,12 +2,13 @@
 
 
 <head>
-<title>Title</title>
+  <title>Title</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="Put your description here.">
+  <meta charset="UTF-8">
 
 </head>
-<body>
+<body style="font-size:5vh; padding:5vh; text-align:center;">
   Hello World!
 </body>
 </html>
